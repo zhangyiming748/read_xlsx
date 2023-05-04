c59c5b4bf54a5d36eb356eb206f180c43e29bc66
zen <zhangyiming748@gmail.com>
2023-05-04 22:09:44 +0800

    init

---
+++ go.mod
@@ -0,0 +1,19 @@
+module read_xlsx
+
+go 1.20
+
+require (
+	github.com/frankban/quicktest v1.14.5 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/rogpeppe/fastuuid v1.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/shabbyrobe/xmlwriter v0.0.0-20220218224045-defe0ad214f6 // indirect
+	github.com/tealeg/xlsx/v3 v3.2.4 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/tealeg/xlsx/v3"
+	"golang.org/x/exp/slog"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("acode.xlsx")
+	all, err := io.ReadAll(file)
+	if err != nil {
+		return
+	}
+	binary, err := xlsx.OpenBinary(all)
+	if err != nil {
+		return
+	}
+	sheet := binary.Sheets[0]
+	rowNum := sheet.MaxRow //行
+	//简单校验一下excel里内容是否是测试用例模板
+	checkRow, _ := sheet.Row(0)
+	if checkRow.GetCell(0).Value != "中文名" {
+		slog.Info("not equal")
+	} else {
+		slog.Info("equal")
+	}
+	openFile, err := os.OpenFile("exam.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		return
+	}
+	defer openFile.Close()
+
+	for i := 1; i < rowNum; i++ {
+		row, _ := sheet.Row(i)
+		name := row.GetCell(0).Value
+		code := row.GetCell(1).Value
+		slog.Info("获取当前行", slog.String("name", name), slog.String("code", code))
+		s := strings.Join([]string{"\"", name, "\":\"", code, "\",\n"}, "")
+		openFile.WriteString(s)
+	}
+}
